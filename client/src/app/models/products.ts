54d0d2c6ae0fee04782006612b502dffa42d287a
unknown <popovic.teodora@ymail.com>
2024-06-04 09:24:53 +0200

    Sephora 1.9

---
+++ client/src/app/models/products.ts
@@ -0,0 +1,12 @@
+
+  
+  export interface Product {
+    id: number
+    name: string
+    description: string
+    price: number
+    brand: string
+    productType: string
+    available: number
+    imageUrl: string
+  }
