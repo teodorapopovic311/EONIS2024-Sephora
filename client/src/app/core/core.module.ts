0255fb8b6b67801928d28c0fae504817c0c31951
unknown <popovic.teodora@ymail.com>
2024-06-08 19:54:31 +0200

    Additions

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -7,6 +7,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
 import { ToastrModule } from 'ngx-toastr';
 import { SectionHeaderComponent } from './section-header/section-header.component';
+import { BreadcrumbModule } from 'xng-breadcrumb';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 
 
@@ -24,11 +26,14 @@ import { SectionHeaderComponent } from './section-header/section-header.componen
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
       preventDuplicates: true
-    })
+    }),
+    BreadcrumbModule,
+    NgxSpinnerModule
   ],
   exports: [
     NavBarComponent,
-    SectionHeaderComponent
+    SectionHeaderComponent,
+    NgxSpinnerModule
   ]
 })
 export class CoreModule { }
