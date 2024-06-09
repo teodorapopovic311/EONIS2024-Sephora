fb32cfbd75960c5aaaa418ed6e8385d3352a0d0f
unknown <popovic.teodora@ymail.com>
2024-06-09 05:47:26 +0200

    Account

---
+++ client/src/app/shared/models/user.ts
@@ -0,0 +1,22 @@
+export interface User {
+
+        Email: string;
+        DisplayName: string;
+        Token: string;
+        Role: string
+        
+}
+
+export interface Address {
+
+        FirstName: string;
+
+        LastName: string;
+
+        Street: string;
+
+        City: string;
+
+        PhoneNumber: string;
+
+} 
